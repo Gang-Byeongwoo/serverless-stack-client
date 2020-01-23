b592a316925fb6e35372ae8db569a234054a5952
Gang Byeong-woo <dudnf43@naver.com>
2020-01-23 10:39:47 +0900

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "ap-northeast-2",
+      BUCKET: "gangbyeongwoo"
+    },
+    apiGateway: {
+      REGION: "ap-northeast-2",
+      URL: "https://isiip32qg7.execute-api.ap-northeast-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "ap-northeast-2",
+      USER_POOL_ID: "ap-northeast-2_WZxq50gVI",
+      APP_CLIENT_ID: "3ov5qa48juq6mql4r6qr5rta8c",
+      IDENTITY_POOL_ID: "ap-northeast-2:44d66a9a-c2dc-412b-8627-221917a80617"
+    }
+  };
